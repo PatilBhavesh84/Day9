448ef05fc118ba9bad1e9e21c763d5d45fa851e1
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-19 19:46:45 +0530

    Added:Ability to add new contact

--- src/com/BridgeLabz/day9/AddressBook.java
+++ src/com/BridgeLabz/day9/AddressBook.java
@@ -4,10 +4,9 @@ import java.util.*;
 
 public class AddressBook {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book program ");
-		String firstName,lastName,address,city,state,emailId,phoneNumber;
-		int zip;
+	String firstName,lastName,address,city,state,emailId,phoneNumber;
+	int zip;
+	void getDetails() {
 		Scanner sc=new Scanner(System.in);
 
 		System.out.println("Enter first name: ");
@@ -16,36 +15,39 @@ public class AddressBook {
 		System.out.println("Enter last name: ");
 		lastName=sc.nextLine();
 
-        System.out.println("Enter address: ");
-        address=sc.nextLine();
-        
-        System.out.println("Enter city: ");
-        city=sc.nextLine();
-        
-        System.out.println("Enter state: ");
-        state=sc.nextLine();
-        
-        System.out.println("Enter phone number");
-        phoneNumber=sc.nextLine();
-        
-        System.out.println("Enter email id: ");
-        emailId=sc.nextLine();
-                  
-        System.out.println("Enter zip code");
-        zip=sc.nextInt();
-        sc.close();
-        
-        
-        //printing output  
-        
-        
-        System.out.println("Name:"+firstName+" "+lastName);
-        System.out.println("Address:"+address);
-        System.out.println("City:"+city);
-        System.out.println("State:"+state);
-        System.out.println("Phone Number:"+phoneNumber);
-        System.out.println("Email Id:"+emailId);
-         
+		System.out.println("Enter address: ");
+		address=sc.nextLine();
 
+		System.out.println("Enter city: ");
+		city=sc.nextLine();
+
+		System.out.println("Enter state: ");
+		state=sc.nextLine();
+
+		System.out.println("Enter phone number");
+		phoneNumber=sc.nextLine();
+
+		System.out.println("Enter email id: ");
+		emailId=sc.nextLine();
+
+		System.out.println("Enter zip code");
+		zip=sc.nextInt();
+		sc.close();
+
+	}
+	//printing output  
+
+	void display() {
+		System.out.println("Name:"+firstName+" "+lastName);
+		System.out.println("Address:"+address);
+		System.out.println("City:"+city);
+		System.out.println("State:"+state);
+		System.out.println("Phone Number:"+phoneNumber);
+		System.out.println("Email Id:"+emailId);
+	}
+	public static void main(String []args) {
+		AddressBook obj=new AddressBook();
+		obj.getDetails();
+		obj.display();
 	}
 }
