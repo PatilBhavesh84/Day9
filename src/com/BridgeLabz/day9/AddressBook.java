fdf4dc34b4ae5e680e3b9ca992e526b6f3444796
PatilBhavesh84 <107094696+PatilBhavesh84@users.noreply.github.com>
2022-07-28 19:57:17 +0530

    Update AddressBook.java

--- src/com/BridgeLabz/day9/AddressBook.java
+++ src/com/BridgeLabz/day9/AddressBook.java
@@ -1,23 +1,16 @@
 package com.BridgeLabz.day9;
-
-//import javax.print.event.PrintJobListener;
-
 class AddressBook{
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address book Program");
-		ContactBook Amit = new ContactBook();
-		Amit.getDetails();
-		System.out.println("Amit Details are :");
-		Amit.display();
-//		Amit.ChangingCity("Kerla");
-//		Amit.display();
-//		Deleting Contact details
-//		Amit = nul;
-		//Creating new Contact
-		ContactBook Rahul = new ContactBook();
-		Rahul.getDetails();
-		System.out.println("Rahul Details are :");
-		Rahul.display();
+		ContactBook Bhavesh = new ContactBook();
+		Bhavesh.getDetails();
+		System.out.println("Bhavesh's Details are :");
+		Bhavesh.display();
+
+		ContactBook Vinay = new ContactBook();
+		Vinay.getDetails();
+		System.out.println("Vinay's Details are :");
+		Vinay.display();
 	}  
 }
