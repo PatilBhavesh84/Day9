06768f41c8ebdceac49044c74e3008bee00ece65
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-08-07 20:08:54 +0530

    Added:Search person by city

--- src/com/BridgeLabz/day9/AddressBook.java
+++ src/com/BridgeLabz/day9/AddressBook.java
@@ -1,16 +1,59 @@
 package com.BridgeLabz.day9;
 
-public class AddressBook {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address book Program");
-		ContactBook Bhavesh = new ContactBook();
-		Bhavesh.getDetails();
-		System.out.println("Bhavesh Details are :");
-		Bhavesh.display();
+//import java.util.Scanner;
 
-		ContactBook Vinay = new ContactBook();
-		Vinay.getDetails();
-		System.out.println("Vinay Details are :");
-		Vinay.display();
+public class AddressBook {
+	String firstName,lastName,address,city,state,emailId,zip,phoneNumber;
+		
+	AddressBook (String  firstName, String  lastName, String  address, String  city, String  state,
+	           String  zip,String  emailId, String  phoneNumber){
+		this.firstName=firstName;
+      this.lastName=lastName;
+      this.address=address;
+      this.city=city;
+      this.state=state;
+      this.zip=zip;
+      this.emailId=emailId;
+      this.phoneNumber=phoneNumber;
 	}
+
+	void display() {
+	System.out.println(firstName+" "+lastName+" Details");
+	System.out.println("Address: "+address);
+	System.out.println("City: "+city);
+	System.out.println("State: "+state);
+	System.out.println("Zip: "+zip);
+	System.out.println("Phone Number: "+phoneNumber);
+	System.out.println("Email Id: "+emailId);
+	}
+
+	public void ChangingFirstName (String n){
+		 firstName = n; 
+	}
+	public void ChangingLastName (String n){
+		 lastName = n; 
+	}
+	public void ChangingAddress (String n){
+		 address = n; 
+	}
+	public void ChangingCity(String n){
+		 city = n; 
+	}
+	public void ChangingState(String n){
+		 state = n; 
+	}
+	public void ChangingEmailName (String n){
+		 emailId = n; 
+	}
+	public void ChangingZip (String n){
+		 zip = n; 
+	}
+	public void ChangingPhoneNumber (String n){
+		 phoneNumber = n; 
+	}
+	
+  public String toString() {
+      return	 firstName+" "+lastName+" Details "+"\nfirstName: " + firstName + "\nLastName: " + lastName + "\nAddress: "
+              + address + "\nCity :"+city+"\nState: "+state+"\nEmailID: " +emailId+"\nPhoneNumber: "+ phoneNumber+"\nZip: "+zip;
+  }
 }
--- src/com/BridgeLabz/day9/ContactBook.java
+++ src/com/BridgeLabz/day9/ContactBook.java
@@ -1,63 +1,49 @@
 package com.BridgeLabz.day9;
 
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ContactBook {
-	String firstName,lastName,address,city,state,emailId,phoneNumber;
-	int zip;
-	public void getDetails() {
-		System.out.println("Enter new contact details");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first Name");
-		firstName = sc.nextLine();
-		System.out.println("Enter Last Name");
-		lastName=sc.nextLine();
-		System.out.println("Enter Address ");
-		address=sc.nextLine();
-		System.out.println("Enter City Name");
-		city=sc.nextLine();
-		System.out.println("Enter State Name");
-		state=sc.nextLine();
-		System.out.println("Enter Email ID");
-		emailId=sc.nextLine();
-		System.out.println("Enter Phone number");
-		phoneNumber=sc.nextLine();
-		System.out.println("Enter Zip");
-		zip=sc.nextInt();
-	}
+public class ContactBook  {
 
-	public void display() {
-		System.out.println(firstName+" "+lastName+" Details");
-		System.out.println("Address: "+address);
-		System.out.println("City: "+city);
-		System.out.println("State: "+state);
-		System.out.println("Zip: "+zip);
-		System.out.println("Phone Number: "+phoneNumber);
-		System.out.println("Email Id: "+emailId);
-	}
+	public static void main(String[] args) {
 
-	public void ChangingFirstName (String n){
-		firstName = n; 
-	}
-	public void ChangingLastName (String n){
-		lastName = n; 
-	}
-	public void ChangingAddress (String n){
-		address = n; 
-	}
-	public void ChangingCity(String n){
-		city = n; 
-	}
-	public void ChangingState(String n){
-		state = n; 
-	}
-	public void ChangingEmailName (String n){
-		emailId = n; 
-	}
-	public void ChangingPhoneNumber (String n){
-		phoneNumber = n; 
-	}
-	public void ChangingZip (int n){
-		zip = n; 
+		Map<String,AddressBook> map=new HashMap<String,AddressBook>();    
+
+		AddressBook Bhavesh = new AddressBook("Bhavesh","Patil","44 Vasant nagar ","Shahada","Maharashtra",
+				"425409","bhavesh@gmail.com","9975543019");   
+		AddressBook Vinay = new AddressBook("Vinay","Shimpi","40 Govind nagar","Prakasha","Maharashtra",
+				"425411","vinay4@gmail.com","9954565124");   
+		AddressBook Pulkit = new AddressBook ("Pulkit","Chopra","33 Vijay nagar","Indore","Madhya Pradesh",
+				"432112","pulkit12@gmail.com","8864531246");    
+		AddressBook Ritik = new AddressBook ("Ritik","Manglani","12 Sindhi Colony","Nandurbar","Maharashtra",
+				"432112","pulkit12@gmail.com","7798457166"); 
+		map.put("Bhavesh",Bhavesh);  
+		map.put("Vinay",Vinay);  
+		map.put("Pulkit",Pulkit);  
+		map.put("Ritik", Ritik);
+		for(Map.Entry<String,AddressBook> entry:map.entrySet()){    
+
+			AddressBook p = entry.getValue();  
+
+			System.out.println(p.firstName+" "+p.lastName+" Details \nfirstName: "+p.firstName +"\nLastName: " +p.lastName + "\nAddress: "
+					+ p.address + "\nCity :"+p.city+"\nState: "+p.state+"\nEmailID: " +p.emailId+"\nPhoneNumber: "+ p.phoneNumber+"\nZip: "+p.zip);   
+			System.out.println();
+		}
+
+		int countOfPersonByCityName = 0;
+		for(Map.Entry<String,AddressBook> entry:map.entrySet()){    
+
+			AddressBook p = entry.getValue();
+
+			if (p.city.equals("Mumbai")) {
+				System.out.println(p.firstName+" "+p.lastName+" Details \nfirstName: "+p.firstName +"\nLastName: " +p.lastName + "\nAddress: "
+						+ p.address + "\nCity :"+p.city+"\nState: "+p.state+"\nEmailID: " +p.emailId+"\nPhoneNumber: "+ p.phoneNumber+"\nZip: "+p.zip);   
+				System.out.println();
+				countOfPersonByCityName++;
+			}
+		}
+		if(countOfPersonByCityName==0) {
+			System.out.println("Record not found");
+		}
 	}
 }
